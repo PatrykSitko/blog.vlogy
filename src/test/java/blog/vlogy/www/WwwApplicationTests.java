111e41d31273ac9b54206fb32753f9bd43b2c15c
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2023-08-21 22:19:25 +0200

    initial commit

---
+++ src/test/java/blog/vlogy/www/WwwApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.vlogy.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
